6437c1de64e9e6df86b7a49e5722b656eabbae8b
shinji46kuro <shinji.46.kuro@gmail.com>
2018-11-18 14:33:33 +0900

    Initial Commit

---
+++ UserDefaultsMock/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  UserDefaultsMock
+//
+//  Created by Shinji Kurosawa on 2018/11/18.
+//  Copyright © 2018 Shinji Kurosawa. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
